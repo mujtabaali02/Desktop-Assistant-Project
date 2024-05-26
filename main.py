562043a803825967f8e463fdce250bfd0a004fca
Mujtaba Ali <alimujtaba9012@gmail.com>
2024-05-26 17:21:20 +0530

    updated main.py

--- main.py
+++ main.py
@@ -97,10 +97,26 @@ if __name__ == "__main__":
             webbrowser.open("github.com")
 
         elif "up validation tracker" in query:
-            speak("Opening undelvalidationtracker for uttar pradesh india")
+            speak("Opening undelvalidationtracker for uttar pradesh Region")
             webbrowser.open("https://docs.google.com/spreadsheets/d/1Spd8DtzpmOLpDyE0OK-iKw2Wx1sylpCIHW3IvH0f1eg/edit#gid=998157805")
             # webbrowser.open("amazon.com")
         
+        elif "RJHR validation tracker" in query:
+            speak("Opening undel validation tracker for Rajasthan and Haryana Region")
+            webbrowser.open("https://docs.google.com/spreadsheets/d/1NWrZ5THXhWLsxEDyIXLxiYjU6L5Y0CaSPFLC1jVNXkg/edit#gid=1216455963")
+        
+        elif "Upper North validation tracker" in query:
+            speak("Opening undel validation tracker for Upper North Region")
+            webbrowser.open("https://docs.google.com/spreadsheets/d/14rO-QCRlicdLgk4N2yvh9dWCvcgEY42fdFLiYxXICD0/edit#gid=0")
+        
+        elif "DLNCR validation tracker" in query:
+            speak("Opening undel validation tracker for Delhi NCR...")
+            webbrowser.open("https://docs.google.com/spreadsheets/d/1XApzET25dDu-Q7DVn6s2iu4kGJAhydYq2uVW8_ijL4A/edit#gid=0")
+        
+        elif "GJMP validation tracker" in query:
+            speak("Opening undelvalidationtracker for Gujarat and Madhya Pradesh Region")
+            webbrowser.open("https://docs.google.com/spreadsheets/d/1UApdetLHxF5d2qKwyK_v118KvgD5Rc9lYHxCi0OZ_so/edit#gid=0") 
+
          #This query for say the times
         elif 'time' in query:
             strTime = datetime.datetime.now().strftime("%H:%M:%S")
